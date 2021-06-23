c2aac73d6be9db76c21503cd4e0763fa1545f826
michael-act <fctroll85@gmail.com>
2021-06-23 22:55:02 +0700

    Done: URL Shortener

--- URL Shortener/README.md
+++ URL Shortener/README.md
@@ -1,1 +1,26 @@
-# [URL Shortener Microservice](https://www.freecodecamp.org/learn/apis-and-microservices/apis-and-microservices-projects/url-shortener-microservice)
+## Install & Run
+
+```
+$ git clone https://github.com/michael-act/FreeCodeCamp
+$ cd FreeCodeCamp/URL\ Shortener
+$ npm install
+$ npm start
+```
+
+#### Try your first Notes Backend program
+
+```shell
+$ curl -X POST "url=\"https://google.com/\"" HOST_IP:PORT/api/shorturl
+{
+  "1343": "https://google.com/",
+  "#": "#"
+}
+
+$ curl -X POST "url=\"https://instagram.com/\"" HOST_IP:PORT/api/shorturl
+{
+  "1343": "https://google.com/", 
+  "4952": "https://instagram.com", 
+  "#": "#"
+}
+```
+Please understand the codes in `./src` for further understanding. 
--- URL Shortener/server.js
+++ URL Shortener/server.js
@@ -3,10 +3,15 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 
+const bodyParser = require('body-parser');
+
 // Basic Configuration
 const port = process.env.PORT || 3000;
 
 app.use(cors());
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 app.use('/public', express.static(`${process.cwd()}/public`));
 
@@ -18,6 +23,56 @@ app.get('/', function(req, res) {
 app.get('/api/hello', function(req, res) {
   res.json({ greeting: 'hello API' });
 });
+
+// Handle long URL to short URL API processing
+const listShortURL = {'#': "#"}
+
+function genUniqueID(arr, callback) {
+	let id = '#'
+	setTimeout(() => {
+		while (arr.includes(id)) {
+			id = Math.floor(Math.random() * 10000).toString()
+		}
+		callback(id)
+	}, 100)
+}
+
+app.route('/api/shorturl/:id?')
+	.get((req, res) => {
+		const { id } = req.params
+		if (id in listShortURL) {
+			res.redirect(301, listShortURL[id])
+		}
+	})
+	.post((req, res) => {
+		const { url } = req.body
+
+		const urlRegex = /^(https?:\/\/)[\w.-]+(?:\.[\w\.-]+)+[\w\-\._~:/?#[\]@!\$&'\(\)\*\+,;=.]+$/
+		if (urlRegex.test(url) === false) {
+			res.json({error: 'invalid url'})
+			return
+		}
+
+		const keys = Object.keys(listShortURL)
+		const values = Object.values(listShortURL)
+
+		const idurl = values.indexOf(url)
+		if (idurl > -1) {
+			res.json({
+				original_url: values[idurl], 
+				short_url: parseInt(keys[idurl])
+			})
+		} else {
+			genUniqueID(keys, id => {
+				Object.assign(listShortURL, {[id]: url})
+				console.log(listShortURL)
+				res.json({
+					original_url: url, 
+					short_url: parseInt(id)
+				})
+			})
+		}
+	})
 
 app.listen(port, function() {
   console.log(`Listening on port ${port}`);
